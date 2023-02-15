ea71bfc8f4ce0405d168e6f5c4078fffd714c07b
JooJaAng <125470215+JooJaAng@users.noreply.github.com>
2023-02-15 10:45:44 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+let numero = 1;
+
+{
+  let numero = 2;
+  console.log("dentro = ", numero);
+}
+
+console.log("fora = ", numero);
+
+//declarando variável com var
+
+var numero1 = 1;
+
+{
+  var numero1 = 2;
+  console.log("dentro1 = ", numero1);
+}
+
+console.log("fora1 = ", numero1);
+
+//declarando uma constante
+
+//const numero2 = 10
+
+//numero2 = 11let numero = 1
+
+{
+  let numero = 2;
+  console.log("dentro = ", numero);
+}
+
+console.log("fora = ", numero);
+
+//declarando variável com var
+
+var numero1 = 1;
+
+{
+  var numero1 = 2;
+  console.log("dentro1 = ", numero1);
+}
+
+console.log("fora1 = ", numero1);
+
+//declarando uma constante
+
+//const numero2 = 10
+
+//numero2 = 11
